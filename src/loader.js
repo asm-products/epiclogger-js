e3b3a0c79e4303ddaad751688a02f2a033ecb1d3
Asankhaya Sharma <asankhaya@yahoo.com>
2015-04-13 16:27:15 +0800

    Clean up - removed jquery and bootstrap.

--- src/loader.js
+++ src/loader.js
@@ -1,8 +1,6 @@
 //@codekit-prepend "json2.js"
 //@codekit-append "loader-code.js"
 //@koala-prepend "json2.js"
-//@koala-prepend "jquery.min.js"
-//@koala-prepend "bootstrap.min.js"
 //@koala-append "picoModal.js"
 //@koala-append "loader-code.js"
 
